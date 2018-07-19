43e28271303cad0b72da1a4dfdb62905f7b507e6
Alexis Melia <alexis.melia@epitech.eu>
2018-07-19 13:37:42 +0200

    Modif mdp email et username

---
+++ Alexis/view/login.php
@@ -0,0 +1,57 @@
+<?php 
+session_start();
+include '../conn_bdd.php';
+if (isset($_POST['submit'])) 
+{
+	$email = $_POST['email'];
+	$mdp = $_POST['password'];
+	if ($login->login())
+	{
+		session_start();
+		session();
+		$_SESSION['id_user'] = $row['id_user'];
+		$_SESSION['username'] = $row['username'];
+		$_SESSION['firstname'] = $row['firstname'];
+		$_SESSION['lastname'] = $row['lastname'];
+		$_SESSION['email'] = $row['email'];
+		$_SESSION['passsword'] = $row['password'];
+		
+		header('location:index.php');
+	}
+	
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Login</title>
+</head>
+<body>
+	<footer>
+		<h2>Tweet_Academie</h2>
+	</footer>
+	<form method="post">
+		<div class="form-row block">
+			<div class="container">
+				<h2>Connexion</h2>
+				<div class="form-group col-md-6">
+					<input type="email" name="email" placeholder="email" class="form-control">
+				</div>
+					<div class="form-group col-md-6">
+					<input type="password" name="password" placeholder="mot de passe" class="form-control">
+				</div>
+					<div class="form-group col-md-6">
+						<input type="submit" name="submit" value="Connexion" class="btn btn-dark">
+					</div>
+				
+			</div>
+		</div>
+		</form>
+			<div class="form-group col-md-6">
+		<a href="register.php" class="lien">Toujours pas inscrit ?</a>
+	</div>
+</body>
+</html>
---
+++ Alexis/view/profil.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+include '../conn_bdd.php'; 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Mon profil</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+<h1>Le profil de <?php echo $_SESSION['firstname'];?>&nbsp<?php echo $_SESSION['lastname'];?></h1>
+<h3></h3>
+</body>
+</html>
